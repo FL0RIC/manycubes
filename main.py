6db4de2e57af7018db9c416c51d70abd01c65950
Florent Becker <florent54bkr@gmail.com>
2023-01-02 23:23:19 +0100

    essaie nouvelle methode

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ import os
 from time import sleep
 import csv
 
+'''
+
 version = input("version : ")
 
 
 def open_server(version):
-    '''Open a minecraft server with the version wanted'''
+   ''''''Open a minecraft server with the version wanted''''''
     os.chdir(f"minecraft_jar\minecraft_{version}")
-    os.system(f"minecraft_server_{version}.jar")
+    os.system(f"minecraft_server_{version}.jar nogui")
 
 
 if not("eula.txt" in os.listdir(f"minecraft_jar\minecraft_{version}")) : # Check if the version was already opened before
@@ -27,5 +29,10 @@ if not("eula.txt" in os.listdir(f"minecraft_jar\minecraft_{version}")) : # Check
     fichier_eula = open(f"eula.txt", "w")
     fichier_eula.write(contenue)
     fichier_eula.close()
-
+os.system("cmd")
 open_server(version)
+'''
+print(os.listdir("minecraft_jar\minecraft_1.19.3"))
+
+os.system("start.bat")
+print("fin")
---
+++ start.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+cd minecraft_jar\minecraft_1.19.3
+java -Xms1024M -Xmx2048M -jar minecraft_server_1.19.3.jar --nogui
+pause
